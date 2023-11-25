32196210726663192dc1553827b03575c1e38d7f
Peter Bulloch <peterbllch2@gmail.com>
2023-11-25 19:25:50 +0900

    Fix deploy script to fetch changes first

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 ssh -i "../../koteach.pem" ubuntu@ec2-18-133-26-124.eu-west-2.compute.amazonaws.com << EOF
   cd data/koteach
   sudo pkill gunicorn
+  git fetch
   git reset --hard origin/main
   sudo gunicorn main:app --workers 3 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:80 --reload --daemon --log-file koteach.log --access-logfile="koteach.log"
 EOF
