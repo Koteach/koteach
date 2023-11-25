702e903e4aaa460457095ca0110c891a0fdbdea3
Peter Bulloch <peterbllch2@gmail.com>
2023-11-25 18:05:46 +0900

    Add get and delete for hagwons

--- main.py
+++ main.py
@@ -14,42 +14,97 @@ def root():
     return "Welcome to Koteach"
 
 
-class Hagwon(BaseModel):
-    id: int | None = None
-    name: str | None
-    description: str | None
-    location: str | None
-    created_at: datetime | None = None
-    modified_at: datetime | None = None
+class CreateHagwonRequest(BaseModel):
+    name: str
+    description: str
+    location: str
 
 
-@app.post("/hagwon")
-def create_hagwon(hagwon: Hagwon):
-    try:
-        hagwon.created_at
-    except NameError:
-        hagwon.created_at = datetime.now()
+class CreateHagwonResponse(BaseModel):
+    hagwon_id: int
+    message: str
 
-    hagwon.modified_at = datetime.now()
+
+
+@app.post("/hagwon", response_model=CreateHagwonResponse)
+def create_hagwon(hagwon: CreateHagwonRequest):
+    now = datetime.now()
 
     create_hagwon_query = "INSERT INTO hagwon (name, description, location, created_at, modified_at) VALUES (%s, %s, %s, %s, %s)"
-    values = (hagwon.name, hagwon.description, hagwon.location, hagwon.created_at, hagwon.modified_at)
+    values = (hagwon.name, hagwon.description, hagwon.location, now, now)
     db.execute(create_hagwon_query, values)
 
     db_connection.commit()
 
-    return {"hagwon_id": db.lastrowid}
 
 
-@app.get("/hagwon")
-def get_hagwon(id: int | None):
+    return {"hagwon_id": db.lastrowid, "message": "Hagwon successfully created"}
+
+
+
+class GetHagwonResponse(BaseModel):
+    id: int
+    name: str
+    description: str
+    location: str
+    created_at: datetime
+    modified_at: datetime
+
+
+@app.get("/hagwon", response_model=GetHagwonResponse)
+def get_hagwon(id: int):
     if not id:
          raise HTTPException(status_code=400, detail="ID required")
 
-    print(id)
-    get_hagwon_query = "SELECT * FROM hagwon WHERE id = %s"
+    get_hagwon_query = "SELECT id, name, description, location, created_at, modified_at FROM hagwon WHERE id = %s"
     values = (id, )
     db.execute(get_hagwon_query, values)
-    print(db.fetchall())
 
-    return ""
+    hagwon = database_hagwon_to_get_hagwon_response(db.fetchall())[0]
+
+    return hagwon
+
+def database_hagwon_to_get_hagwon_response(rows):
+    response = []
+    for row in rows:
+        response.append({
+            "id": row[0],
+            "name": row[1],
+            "description": row[2],
+            "location": row[3],
+            "created_at": row[4],
+            "modified_at": row[5]
+        })
+    return response
+
+
+
+class DeleteHagwonResponse(BaseModel):
+    message: str
+
+
+
+@app.delete("/hagwon", response_model=DeleteHagwonResponse)
+def delete_hagwon(id: int):
+    if not id:
+         raise HTTPException(status_code=400, detail="ID required")
+
+    get_hagwon_query = "DELETE FROM hagwon WHERE id = %s"
+    values = (id, )
+    db.execute(get_hagwon_query, values)
+
+    db_connection.commit()
+
+    return {"message": "Successfully deleted hagwon"}
+
+
+
+@app.get("/hagwons", response_model=list[GetHagwonResponse])
+def get_hagwons(limit: int):
+    get_hagwons_query = "SELECT id, name, description, location, created_at, modified_at FROM hagwon LIMIT %s"
+    values = (limit, )
+    db.execute(get_hagwons_query, values)
+
+    hagwons = database_hagwon_to_get_hagwon_response(db.fetchall())
+
+    return hagwons
